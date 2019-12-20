9698f0de04038173f610eb09f05ade83d63c544f
benjamin-t-wilson <benjiimanwillson@gmail.com>
2019-12-19 18:34:38 -0700

    added migration for restaurant and items

---
+++ data/migrations/20191219181422_restaurant.js
@@ -0,0 +1,59 @@
+exports.up = function(knex) {
+  return knex.schema
+    .createTable("restaurants", tbl => {
+      tbl.increments();
+      tbl
+        .text("name", 128)
+        .notNullable()
+        .unique();
+      tbl.text("cuisine", 128).notNullable();
+      tbl.text("location", 128).notNullable();
+      tbl
+        .integer("hour_open")
+        .notNullable()
+        .unsigned();
+      tbl
+        .integer("hour_closed")
+        .notNullable()
+        .unsigned();
+      tbl.text("days_open", 128).notNullable();
+      tbl
+        .integer("user_id")
+        .notNullable()
+        .unsigned()
+        .references("id")
+        .inTable("users")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      tbl.text("photo_url");
+    })
+    .createTable("items", tbl => {
+      tbl.increments();
+      tbl
+        .integer("restaurant_id")
+        .notNullable()
+        .unsigned()
+        .references("id")
+        .inTable("restaurants")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      tbl.text("cuisine").notNullable();
+      tbl.text("name").notNullable();
+      tbl.text("photo_url");
+      tbl
+        .integer("rating")
+        .notNullable()
+        .unsigned();
+      tbl.string("review").notNullable();
+      tbl
+        .integer("user_id")
+        .notNullable()
+        .unsigned()
+        .references("id")
+        .inTable("users")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+    });
+};
+
+exports.down = function(knex) {};
