f2e3f9dd6f7fa9e9a3d1458b52cf808bcc602c60
benjamin-t-wilson <benjiimanwillson@gmail.com>
2019-12-19 20:46:40 -0700

    added auth middleware

---
+++ api/routes/auth.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (req.decodedJwt) {
+    next();
+  } else if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedJwt) => {
+      if (err) {
+        res.status(401).json({ message: "Failed to verify authorization" });
+      } else {
+        req.decodedJwt = decodedJwt;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: "Failed to verify authorization" });
+  }
+};
